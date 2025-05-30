300bf03b280341b4fe72dd566fbe225378f2beff
Arif Mahmud Rana <amrana83@gmail.com>
2025-05-30 22:49:15 +0600

    wordCount func implementation

---
+++ src/helpers/wordCount.ts
@@ -0,0 +1,9 @@
+export function countWords(text: string): number {
+  // Remove punctuation and convert to lowercase
+  const cleanText = text.replace(/[^\w\s]/g, '').toLowerCase();
+  
+  // Split by whitespace and filter out empty strings
+  const words = cleanText.split(/\s+/).filter(word => word.length > 0);
+  
+  return words.length;
+}

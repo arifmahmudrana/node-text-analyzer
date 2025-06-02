f7622472de3b9c1538768bbea0570482635b9f76
Arif Mahmud Rana <amrana83@gmail.com>
2025-06-02 11:13:50 +0600

    remove unused Document import

--- src/models/Text.ts
+++ src/models/Text.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { TextDocument } from '../types';
 
 const TextSchema = new Schema<TextDocument>({
